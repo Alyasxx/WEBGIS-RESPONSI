3c58eed7fd0e2e51104ba6177cb7eed5f44deb1b
Alyasxx <alyasyahidha@mail.ugm.ac.id>
2023-11-24 23:42:25 +0700

    modify

--- addpoint.php
+++ addpoint.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CHICKEN'S MAP</title>
+    <title>FORMS ADD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
@@ -154,7 +154,7 @@
     <!-- footer -->
     <footer class="bg-dark p-2 text-center">
         <div class="container">
-        <p class="text-light">Alya Syahidha &copy; 2023 - WEBGIS | RESPONSE</p>
+            <p class="text-light">Alya Syahidha &copy; 2023 - WEBGIS | RESPONSE</p>
         </div>
     </footer>
     </script>
